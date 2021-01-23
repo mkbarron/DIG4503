81f17cc76651297db2d0b1e3226134738cc2f0c4
mkbarron <59653472+mkbarron@users.noreply.github.com>
2021-01-23 02:56:46 -0500

    lab 1
    forgot to submit :(

---
+++ lab1/index.js
@@ -0,0 +1,5 @@
+const Michelle = require("./michelle.js");
+
+let p = new Michelle ("Michelle", "orange");
+
+p.speak();
---
+++ lab1/michelle.js
@@ -0,0 +1,9 @@
+const Person = require("./person.js");
+
+class Michelle extends Person {
+    constructor(name, color) {
+        super(name, color);
+    }
+}
+
+module.exports = Michelle;
