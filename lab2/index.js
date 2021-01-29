6c6935e8b77d9ca97242bcfedad27e15cbac2481
mkbarron <59653472+mkbarron@users.noreply.github.com>
2021-01-29 17:52:57 -0500

    lab 2 commit
    take a ride on my magikarp

---
+++ lab2/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+// Fetches pokemon api through axios promise statement
+axios('https://pokeapi.co/api/v2/pokemon/magikarp')
+    .then(function (response) {
+        // Uses the 'data' provided from specified api
+        const pokemon = response.data;
+
+        // In this example, the above URL will return an object
+        //  pokemon api will return an object with the data on magikarp since it was requested
+        console.log("This is a " + pokemon.name + " and its ID is " + pokemon.id);
+    })
+    .catch(function (error) {
+        // handles errors such as mispelling or if pokemon is missing from data
+        console.log("Error: " + error);
+    });
