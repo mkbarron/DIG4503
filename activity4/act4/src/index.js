ac0664cf061b89b28627ebd2765197a3d2bc5388
mkbarron <59653472+mkbarron@users.noreply.github.com>
2021-02-05 21:16:32 -0500

    act 4

---
+++ activity4/act4/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+const customer = {
+
+  first_name: "Michelle",
+
+  last_name: "Barron"
+
+};
+
+//              your code inside these <div> tags
+
+const output = <div></div>;
+
+ReactDOM.render(output, document.querySelector("#root"));
