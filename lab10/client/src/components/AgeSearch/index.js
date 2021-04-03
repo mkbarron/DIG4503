1b732d32b8b55e78a76dc14c43824551435bb410
mkbarron <59653472+mkbarron@users.noreply.github.com>
2021-04-02 21:31:21 -0400

    lab10

---
+++ lab10/client/src/components/AgeSearch/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+class AgeSearch extends React.Comonent {
+    readNumber(event){
+        event.preventDefault();
+            let element = document.querySelector("#number");
+
+                fetch("/api/ages/:number/" + element.value)
+                .then((res) => {
+                return res.json();
+                })
+
+            .then((processed) => {
+            let reporting = document.querySelector ("#reportingArea");
+                if(processed.error) {
+                    reporting.innerHTML = processed.error;
+                } else {
+                    reporting.innerHTML = processed.name;
+                }
+                });
+   element.value = "";
+    }
+    rener() {
+        return (
+            <div>
+                <h2> Age </h2>
+                <form onSubmit= {this.readNumber}>
+                    <input id="number" type ="text"/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+export default AgeSearch;
